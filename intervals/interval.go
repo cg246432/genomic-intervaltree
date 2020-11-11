58abb2702405650f4d4a0506cb81ad67060ba7dc
cgreco206664 <cgreco@genedx.com>
2020-11-11 12:49:48 -0500

    Overlap logic done.

--- intervals/interval.go
+++ intervals/interval.go
@@ -14,11 +14,7 @@ type Interval struct {
 //Overlap determines whether a start and stop overlaps this interval
 func (i Interval) Overlap(begin int, end int) bool {
 	//
-	if !(i.Stop <= begin || i.Start >= end) {
-		fmt.Println(i.Stop <= begin)
-		fmt.Println(i.Start >= end)
-		fmt.Println(!(false || false))
-		fmt.Println(i.Start, i.Stop, begin, end)
+	if i.Stop <= begin || i.Start >= end {
 		return false
 	}
 	return true
