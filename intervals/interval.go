ff09339dc6e7884792c1eccd8e6ae7bfa878b28f
cgreco206664 <cgreco@genedx.com>
2020-11-11 13:29:14 -0500

    Added comments and DeleteInterval function.

--- intervals/interval.go
+++ intervals/interval.go
@@ -7,6 +7,7 @@ type Interval struct {
 	Data  map[string]interface{}
 }
 
+// It's required for genomic intervals to be sorted
 func (i *Interval) checkorder() {
 	if i.Start > i.Stop{
 		i.Start, i.Stop = i.Stop, i.Start
@@ -23,9 +24,13 @@ func NewInterval(start int, stop int) Interval{
 	return newInterval
 }
 
+// DeleteInterval sets the interval back to empty
+func (i *Interval) DeleteInterval() {
+	i = &Interval{}
+}
+
 //Overlap determines whether a start and stop overlaps this interval
 func (i Interval) Overlap(begin int, end int) bool {
-	i.checkorder()
 	if i.Stop <= begin || i.Start >= end {
 		return false
 	}
@@ -35,7 +40,6 @@ func (i Interval) Overlap(begin int, end int) bool {
 
 // OverlapSize returns size of overlap
 func (i Interval) OverlapSize(begin int, end int) int {
-	i.checkorder()
 	if i.Overlap(begin, end){
 		var minOverlap int
 		minOverlap = i.Stop - i.Start
