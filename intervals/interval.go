e663da307721094668c5d61c08043a7becff5a03
cgreco206664 <cgreco@genedx.com>
2020-11-11 13:07:54 -0500

    Added check order method.

--- intervals/interval.go
+++ intervals/interval.go
@@ -7,9 +7,15 @@ type Interval struct {
 	Data  map[string]interface{}
 }
 
+func (i *Interval) checkorder() {
+	if i.Start > i.Stop{
+		i.Start, i.Stop = i.Stop, i.Start
+	}
+}
+
 //Overlap determines whether a start and stop overlaps this interval
 func (i Interval) Overlap(begin int, end int) bool {
-	//
+	i.checkorder()
 	if i.Stop <= begin || i.Start >= end {
 		return false
 	}
@@ -19,6 +25,7 @@ func (i Interval) Overlap(begin int, end int) bool {
 
 // OverlapSize returns size of overlap
 func (i Interval) OverlapSize(begin int, end int) int {
+	i.checkorder()
 	if i.Overlap(begin, end){
 		var minOverlap int
 		minOverlap = i.Stop - i.Start
